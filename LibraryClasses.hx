2579c13e7cd0aed81be3c5a584fe463c7e050f05
Hans Häggström <zzorn@iki.fi>
2010-10-17 01:42:40 +0300

    Removng automatically generated file

--- LibraryClasses.hx
+++
@@ -1,5 +0,0 @@
-import flash.display.Bitmap;
-
-class Iso_tile_template_png extends Bitmap { public function new() { super(); } }
-class Brown_tile_png extends Bitmap { public function new() { super(); } }
-class Green_floor_png extends Bitmap { public function new() { super(); } }
