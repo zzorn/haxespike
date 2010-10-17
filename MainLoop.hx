472fee64cf0513e199c3a4fd1badbfe97af14c74
Hans Häggström <zzorn@iki.fi>
2010-10-17 14:29:14 +0300

    A sprite class and a mainloop.

---
+++ MainLoop.hx
@@ -0,0 +1,107 @@
+import flash.display.Sprite;
+import flash.display.BitmapData;
+import flash.display.Bitmap;
+import flash.geom.Rectangle;
+import flash.geom.Point;
+
+import flash.events.Event;
+import flash.events.KeyboardEvent;
+import flash.ui.Keyboard;
+
+// Main lopp class.
+// Extending Sprite to get double buffering, event and child object support.
+class MainLoop extends Sprite
+{
+   static public function main()
+   {
+      // TODO: Load and initialize any needed data  
+
+      // Start      
+      new MainLoop();
+   }
+
+
+   var screen: BitmapData;
+
+   // State for each key
+   var keyDown: Array<Bool> = [];
+
+   // Used to move monsters and the player, etc.  Independent from frame rate.
+   var logicUpdatesPerSecond: Float = 15;
+
+   var lastUpdated: Float;
+
+   function new(screenW: Int, screenH: Int) {
+      // Call sprite constructor
+      super();
+      
+      // Display ourselves
+      flash.Lib.current.addChild(this);
+
+      // Create and show screen bitmap
+      screen = new BitmapData(screenW, screenH);
+      addChild(new Bitmap(screen));
+
+      // Update key state
+      stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown );
+      stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp );
+
+      // TODO: Mouse listening
+
+      // Call render frame on each flash frame
+      stage.addEventListener(Event.ENTER_FRAME, onFrame);
+
+      // Init update timestamp  
+      lastUpdated = haxe.Timer.stamp();
+   }
+
+
+   function onKeyDown(event:KeyboardEvent)
+   {
+      if (!keyDown[event.keyCode])
+      {
+         keyDown[event.keyCode] = true;
+      }
+   }
+
+   function onKeyUp(event:KeyboardEvent)
+   {
+      keyDown[event.keyCode] = false;
+   }
+
+   function onFrame(e:flash.events.Event)
+   {
+      var now = haxe.Timer.stamp();
+
+      // Number of logic update steps
+      var steps = Math.floor( (now - lastUpdated) * logicUpdatesPerSecond );
+      lastUpdated += steps * 1 / logicUpdatesPerSecond;
+
+      for(i in 0...steps) updateLogic();
+
+      // Lock screen to do a more efficient batched bitmap update.
+      screen.lock();
+      render();
+      screen.unlock();
+   }
+
+   // Update the game logic one step.
+   function updateLogic()
+   {
+       // TODO: Update game logic 
+   }
+
+   // Draw current view of game
+   function render()
+   {
+      // Clear to bg color
+      screen.fillRect(new Rectangle(0,0,screen.width, screen.height),0x000000);
+      // TODO: Maybe only draw updated parts of the screen?
+
+      // TODO: Draw map  
+
+      // TODO: Update UI
+   }
+
+}
+
---
+++ Picture.hx
@@ -0,0 +1,31 @@
+import flash.display.BitmapData;
+import flash.display.Bitmap;
+import flash.geom.Rectangle;
+import flash.geom.Point;
+
+// Sprite class, gets picture from a spritesheet.
+class Picture
+{
+   var sheet: BitmapData;
+   var area: Rectangle;
+   var location: Point = new Point(0, 0); // Cache point object
+   var hotSpotX: Int;
+   var hotSpotY: Int;
+ 
+   public function new(source:BitmapData, sourceX: Int, sourceY:Int, sourceW:Int, sourceH:Int, sourceHotSpotX: Int, sourceHotSpotY: Int)
+   {
+      sheet = source;
+      area = new Rectangle(sourceX,sourceY,sourceW,sourceH);
+      hotSpotX = sourceHotSpotX;
+      hotSpotY = sourceHotSpotY;
+   }
+
+   public function render(target: BitmapData, x: Int, y: Int)
+   {
+      location.x = x - hotSpotX;
+      location.y = y - hotSpotY;
+      target.copyPixels(sheet,area,location);
+   }
+}
+ 
+
